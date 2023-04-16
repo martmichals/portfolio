4115187aab287ab2bbe99dc7341af428cdfae1de
Martin Michalski <martmichals@gmail.com>
2023-04-16 11:59:47 -0500

    Initial import of assets

---
+++ lib/info.tsx
@@ -0,0 +1,19 @@
+import me from '../app/placeholder.jpg';
+
+export const name = 'Martin Michalski';
+export const avatar = me;
+export const about = () => {
+  return (
+    <>
+      Hey, I'm a Data Scientist who loves Natural Language Processing. I'm currently working at Alight, leading development
+      on a new website search engine.
+    </>
+  );
+};
+export const bio = () => {
+  return (
+    <>
+      When I'm not coding, I'm probably playing video games or watching anime. I also love to travel and explore new places.
+    </>
+  );
+};
